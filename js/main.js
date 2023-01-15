d997fd23315213d7244bd51ca2f27aa9aa49b07e
Pavel Toshchakov <toschakovpv@yandex.ru>
2023-01-16 02:09:33 +0300

    T6 background settings for modal window (not adaptive)

--- js/main.js
+++ js/main.js
@@ -1,20 +1,56 @@
-let btn = document.querySelector("button.button.lear-more.buy");
+let btnBuy = document.querySelector("button.button.lear-more.buy");
+let btnService = document.querySelector("button.button.lear-more.service");
+let btnSale = document.querySelector("button.button.lear-more.sale");
+let btns = [btnBuy, btnService, btnSale];
 let modal = document.querySelector(".modalWrapper");
 let close = document.querySelector(".close");
 let body = document.body;
-let text = document.querySelector(".modalText");
+let back = document.querySelector(".modal-overlay");
 
-btn.addEventListener('click', ()=> {
+let innerModalPic = document.querySelector(".innerModalPhoto");
+let text = document.querySelector(".modalText");
+let modalPhoto = document.querySelector(".modalPhoto");
+
+
+btns.forEach(btn => {
+    btn.addEventListener('click', ()=> {
+    if ('buy' in btn.classList){
+        innerModalPic.insertAdjacentHTML('afterbegin',
+            `<img src='images/buy-modal.png' alt='Buying' className='modalPic'>`);
+    }
+    if ('service' in btn.classList){
+        innerModalPic.insertAdjacentHTML('afterbegin',
+            `<img src='images/service-modal.png' alt='Service' className='modalPic'>`);
+    }
+    if ('sale' in btn.classList){
+            innerModalPic.insertAdjacentHTML('afterbegin',
+                `<img src='images/sale-modal.png' alt='Sale' className='modalPic'>`);
+    }
+
     modal.style.display = 'block';
+    back.style.display = 'flex';
+
     close.addEventListener('click', ()=>{
         modal.style.display = 'none';
+        back.style.display = 'none';
     })
-})
+})})
+
+// btn.addEventListener('click', ()=> {
+//     modal.style.display = 'block';
+//     back.style.display = 'flex';
+//
+//     close.addEventListener('click', ()=>{
+//         modal.style.display = 'none';
+//         back.style.display = 'none';
+//     })
+// })
 
 body.addEventListener('click', (event)=>{
-    if (event.target !== btn &&
+    if (!btns.includes(event.target) &&
         event.target !== modal &&
         event.target !== text){
         modal.style.display = 'none';
+        back.style.display = 'none';
     }
 })
