b1019d49c3e3242e57c13f9bd2a5b84e62cc7d85
Pavel Toshchakov <toschakovpv@yandex.ru>
2023-02-07 17:50:40 +0300

    Pre-Prod version2

--- js/carousel.js
+++ js/carousel.js
@@ -1,27 +1,31 @@
 (function ($) {
     let SlideSpeed = 700;
     let TimeOut = 15000;
-    $(document).ready(function(e) {
+    $(document).ready(function (e) {
         $(".slide").css(
-            {"position" : "absolute",
-                "top":'0', "left": '0'}).hide().eq(0).show();
+            {
+                "position": "absolute",
+                "top": '0', "left": '0'
+            }).hide().eq(0).show();
         let slideNum = 0;
         let slideTime;
         let slideCount = $(".slide").size();
-        let animSlide = function(arrow){
+        let animSlide = function (arrow) {
             clearTimeout(slideTime);
             $(".slide").eq(slideNum).fadeOut(SlideSpeed);
-            if(arrow === "next"){
-                if(slideNum === (slideCount-1)){slideNum=0;}
-                else{slideNum++}
-                console.log(slideNum, slideCount);
-            }
-            else if(arrow === "prew")
-            {
-                if(slideNum === 0){slideNum=slideCount-1;}
-                else{slideNum-=1}
-            }
-            else{
+            if (arrow === "next") {
+                if (slideNum === (slideCount - 1)) {
+                    slideNum = 0;
+                } else {
+                    slideNum++
+                }
+            } else if (arrow === "prew") {
+                if (slideNum === 0) {
+                    slideNum = slideCount - 1;
+                } else {
+                    slideNum -= 1
+                }
+            } else {
                 slideNum = arrow;
             }
             $('.slide').eq(slideNum).fadeIn(SlideSpeed, rotator);
@@ -29,22 +33,31 @@
             $('.control-slide').eq(slideNum).addClass('active');
         }
         let $adderSpan = '';
-        $('.slide').each(function(index) {
+        $('.slide').each(function (index) {
             $adderSpan += '<span class = "control-slide">' + index + '</span>';
         });
-        $('<div class ="slider-controls">' + $adderSpan +'</div>').appendTo('.slider');
+        $('<div class ="slider-controls">' + $adderSpan + '</div>').appendTo('.slider');
         $(".control-slide:first").addClass("active");
-        $('.control-slide').click(function(){
+        $('.control-slide').click(function () {
             let goToNum = parseFloat($(this).text());
             animSlide(goToNum);
         });
         let pause = false;
-        let rotator = function(){
-            if(!pause){slideTime = setTimeout(function(){animSlide('next')}, TimeOut);}
+        let rotator = function () {
+            if (!pause) {
+                slideTime = setTimeout(function () {
+                    animSlide('next')
+                }, TimeOut);
+            }
         }
         $('.slide > div h2').hover(
-            function(){clearTimeout(slideTime); pause = true;},
-            function(){pause = false; rotator();
+            function () {
+                clearTimeout(slideTime);
+                pause = true;
+            },
+            function () {
+                pause = false;
+                rotator();
             });
         rotator();
     });
--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 let btnBuy = document.querySelector("button.button.lear-more.buy");
 let btnService = document.querySelector("button.button.lear-more.service");
 let btnSale = document.querySelector("button.button.lear-more.sale");
-// let btns = [btnBuy, btnService, btnSale];
+
+let head = document.querySelector("head");
+let btns = new Array(...document.querySelectorAll("button.btn_contact"));
+let childs = new Array();
+btns.forEach(value => {
+    childs.push(...value.children)
+})
+let liChilds = new Array();
+childs.forEach(value => {
+    liChilds.push(...value.children);
+})
+
+let lastChild = new Array();
+liChilds.forEach(value => {
+    lastChild.push(...value.children);
+})
+
 
 let modalBuy = document.querySelector("div.modalWrapper.buy");
 let modalService = document.querySelector("div.modalWrapper.service");
@@ -41,28 +57,43 @@ btnSale.addEventListener("click", () => {
     modalSale.style.display = "block";
     backSale.style.display = "flex";
 
-    closeSale.addEventListener("click", () => {
+    closeSale.addEventListener("click", (event) => {
         modalSale.style.display = "none";
         backSale.style.display = "none";
     });
 });
 
 body.addEventListener("click", (event) => {
-    if (event.target !== btnService) {
+    if (event.target !== btnService &&
+        !btns.includes(event.target) &&
+        !childs.includes(event.target) &&
+        !liChilds.includes(event.target) &&
+        !lastChild.includes(event.target)&&
+        event.target !== head) {
         modalService.style.display = "none";
         backService.style.display = "none";
     }
 });
 
 body.addEventListener("click", (event) => {
-    if (event.target !== btnSale) {
+    if (event.target !== btnSale &&
+        !btns.includes(event.target) &&
+        !childs.includes(event.target) &&
+        !liChilds.includes(event.target) &&
+        !lastChild.includes(event.target)&&
+        event.target !== head) {
         modalSale.style.display = "none";
         backSale.style.display = "none";
     }
 });
 
 body.addEventListener("click", (event) => {
-    if (event.target !== btnBuy) {
+    if (event.target !== btnBuy &&
+        !btns.includes(event.target) &&
+        !childs.includes(event.target) &&
+        !liChilds.includes(event.target) &&
+        !lastChild.includes(event.target)&&
+        event.target !== head) {
         modalBuy.style.display = "none";
         backBuy.style.display = "none";
     }
